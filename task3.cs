c93cd74b4ee97df5f4b5b5396b706aa4b3dbdd0b
MinhvaeKeelo <epicsans2000@gmail.com>
2024-09-25 00:01:31 +0300

    Add files via upload

---
+++ task3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int n = int.Parse(Console.ReadLine());
+
+        
+        string[] input = Console.ReadLine().Split();
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = int.Parse(input[i]);
+        }
+
+        
+        if (n > 1) 
+        {
+            int lastElement = array[n - 1];
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                array[i] = array[i - 1];
+            }
+
+            array[0] = lastElement;
+        }
+
+      
+        Console.WriteLine(string.Join(" ", array));
+    }
+}
