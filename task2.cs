58367e6862c19cec893f988ddfa2663d7d1c9d5f
MinhvaeKeelo <epicsans2000@gmail.com>
2024-09-24 23:52:52 +0300

    Add files via upload

---
+++ task2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int n = int.Parse(Console.ReadLine());
+
+        
+        string[] input = Console.ReadLine().Split(' ');
+        int[] array = Array.ConvertAll(input, int.Parse);
+
+        
+        HashSet<int> uniqueElements = new HashSet<int>();
+
+        
+        foreach (int number in array)
+        {
+            uniqueElements.Add(number);
+        }
+
+       
+        Console.WriteLine(uniqueElements.Count);
+    }
+}
