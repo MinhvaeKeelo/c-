8f37239002c478b825d9111afa3fbd434b012b8a
MinhvaeKeelo <epicsans2000@gmail.com>
2024-09-24 23:36:36 +0300

    Add files via upload

---
+++ task1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int n = int.Parse(Console.ReadLine());
+
+        
+        string[] input = Console.ReadLine().Split(' ');
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = int.Parse(input[i]);
+        }
+
+       
+        var result = new List<int>();
+
+        
+        for (int i = 1; i < n; i++)
+        {
+            if (array[i] > array[i - 1])
+            {
+                result.Add(array[i]);
+            }
+        }
+
+      
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
